12acccb5b764cec411e9611972d1f6744eba019e
jehu nieves <jehunieves@gmail.com>
2020-06-28 16:41:05 -0500

    parametros dinamicos y rutas con eventos

--- practica-2/src/views/Fotos.vue
+++ practica-2/src/views/Fotos.vue
@@ -1,7 +1,13 @@
 <template>
     <div>
-        <h1>foto: </h1>
+        <router-link :to="{name:'Fotos', params:{id:item} }" 
+        v-for="(item,index) of fotosArreglo" :key="index" >
+            <button class="btn btn-primary">imagen: {{item}}</button>
+        </router-link>
         <Foto/>
+        <button class="btn btn-danger" @click="home()">home</button>
+        <button class="btn btn-success" @click="anterior()">anterior</button>
+        <button class="btn btn-primary" @click="siguiente()">siguiente</button>
     </div>
 </template>
 
@@ -11,6 +17,22 @@ import Foto from '@/components/Fotografia.vue'
 export default {
     components:{
         Foto
+    },
+    data(){
+        return {
+            fotosArreglo: [1,2,3]
+        }
+    },
+    methods:{
+        home(){
+            this.$router.push('/')
+        },
+        anterior(){
+            this.$router.go(-1)
+        },
+        siguiente(){
+            this.$router.go(1)
+        }
     }
 }
 </script>
