e25c5812c20155db01af1709223825d99695789d
jehu nieves <jehunieves@gmail.com>
2020-07-10 21:14:23 -0500

    bootstrap con vuejs2

--- practica-2/src/App.vue
+++ practica-2/src/App.vue
@@ -1,34 +1,29 @@
 <template>
   <div id="app">
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+
+      <b-container>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-navbar-brand :to="{name:'Home'}">home</b-navbar-brand>
+
+        <b-collapse id="nav-collapse" is-nav>
+
+          <b-navbar-nav>
+            <b-nav-item :to="{name:'Home'}" exact>home</b-nav-item>
+            <b-nav-item :to="{name:'About'}">About</b-nav-item>
+            <b-nav-item :to="{name:'Fotos'}">Fotos</b-nav-item>
+          </b-navbar-nav>
+
+        </b-collapse>
+      </b-container>
+
+    </b-navbar>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/otra">Otra</router-link> |
-      <router-link to="/fotos">Fotos</router-link> |
-    </div>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/otra">Otra</router-link> |
+        <router-link to="/fotos">Fotos</router-link>
+      </div>
     <router-view/>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
