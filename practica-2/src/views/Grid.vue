562afe119779b205056e85159baae5d15e581ccf
jehu nieves <jehunieves@gmail.com>
2020-07-12 14:33:56 -0500

    botones y cards bootrap

--- practica-2/src/views/Grid.vue
+++ practica-2/src/views/Grid.vue
@@ -1,17 +1,53 @@
 <template>
     <div class="mt-5 ml-5 mr-5">
         <b-row>
-            <b-col cols="12" md="6" lg="4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Repellendus, corrupti? Nisi labore fugiat numquam nihil maxime! 
-                Dolorum, rerum minima voluptates autem ipsum 
-                accusantium eius soluta ab nihil placeat doloremque aliquam?
+            <b-col cols="12" md="6" lg="6">
+                <b-card
+                    title="Card Title 1"
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-alt="Image"
+                    img-top
+                    tag="article"
+                    style="max-width: 20rem;"
+                    class="mb-2"
+                >
+                    <b-card-text>
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </b-card-text>
+                    <b-button href="#" variant="info">Go somewhere</b-button>
+                </b-card>
             </b-col>  
-            <b-col cols="12" md="6" lg="8">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Repellendus, corrupti? Nisi labore fugiat numquam nihil maxime! 
-                Dolorum, rerum minima voluptates autem ipsum 
-                accusantium eius soluta ab nihil placeat doloremque aliquam?
+            <b-col cols="12" md="6" lg="6">
+                <b-card
+                    title="Card Title 2"
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-alt="Image"
+                    img-top
+                    tag="article"
+                    style="max-width: 20rem;"
+                    class="mb-2"
+                >
+                    <b-card-text>
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </b-card-text>
+                    <b-button href="#" variant="info">Go somewhere</b-button>
+                </b-card>
+            </b-col> 
+            <b-col cols="12" md="6" lg="6">
+                <b-card
+                    title="Card Title 3"
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-alt="Image"
+                    img-top
+                    tag="article"
+                    style="max-width: 20rem;"
+                    class="mb-2"
+                >
+                    <b-card-text>
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </b-card-text>
+                    <b-button href="#" variant="info">Go somewhere</b-button>
+                </b-card>
             </b-col>  
         </b-row>
     </div>
