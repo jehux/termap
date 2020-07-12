525819be817f4e54f7926c546dfd1d71c842ccfb
jehu nieves <jehunieves@gmail.com>
2020-07-12 15:50:07 -0500

    checkbox radio select

--- practica-2/src/views/Forms.vue
+++ practica-2/src/views/Forms.vue
@@ -2,7 +2,7 @@
 <div class="mt-5 ml-5 mr-5 ">
     <h1>Formularios</h1>
     <b-row>
-        <b-col md="6">
+        <b-col cols="12" md="6" lg="3">
              <b-form-input
                 v-model="texto"
                 type="text"
@@ -11,6 +11,18 @@
                 :state="comprobarCaracteres"
             ></b-form-input>
             <small>minmo 3 caracteres</small>
+        </b-col>
+        <b-col cols="12" md="6" lg="3">
+             <b-form-select v-model="select" :options="animales"></b-form-select>
+            <small>seleccion: {{select}}</small>
+        </b-col>
+        <b-col cols="12" md="6" lg="3">
+             <b-form-radio-group v-model="radio" :options="animalesGroup"></b-form-radio-group>
+            <small>seleccion: {{radio}}</small>
+        </b-col>
+        <b-col cols="12" md="6" lg="3">
+             <b-form-checkbox-group v-model="multiple" :options="animalesGroup"></b-form-checkbox-group>
+            <small>seleccion: {{multiple}}</small>
         </b-col>
     </b-row>
 </div>
@@ -21,7 +33,21 @@ export default {
     name: 'Form',
     data() {
         return {
-            texto:''    
+            texto:'',
+            radio:null,
+            multiple:null,
+            select:null,
+            animales: [
+                {value:null, text:'Seleccione un animal'},
+                {value:'perro', text:'Perro'},
+                {value:'gato', text:'Mishi'},
+                {value:'pato', text:'Pato'},
+                ],
+            animalesGroup: [
+                {value:'perro', text:'Perro'},
+                {value:'gato', text:'Mishi'},
+                {value:'pato', text:'Pato'},
+            ]
         }
     },
     computed: {
